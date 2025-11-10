b92434192425f4a8fa89f622b283ed938f8273d9
tanujal2006 <knvtanuja@yahoo.com>
2025-11-10 11:26:58 -0800

    First chsarp project

---
+++ FirstProject/Firstprogram.cs
@@ -0,0 +1,17 @@
+using System;
+
+class Program
+{
+    static void PrintNumbers()
+    {
+        for (int i = 1; i <= 10; i++)
+        {
+            Console.WriteLine(i);
+        }
+    }
+
+    static void Main()
+    {
+        PrintNumbers();
+    }
+}
